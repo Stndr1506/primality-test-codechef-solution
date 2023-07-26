1f3be3bed32576ddff19ee49170b6897a21bfa97
SATENDRA KUMAR <103168059+Stndr1506@users.noreply.github.com>
2023-07-26 12:57:18 +0530

    Add files via upload

---
+++ PRB01.java
@@ -0,0 +1,31 @@
+/* package codechef; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		for(int i=0;i<t;i++){
+		    int N=sc.nextInt();
+		    int count = 0;
+		    for(int j=1;j<=N;j++){
+		        if(N%j==0){
+		            count++;
+		        }
+		    }
+		    if(count==2){
+		        System.out.println("yes");
+		        
+		    }else{
+		        System.out.println("no");
+		    }
+		}
+	}
+}
